39006149643fed8b19eb2659246560fca3f719a7
Dự Nguyễn Trọng <nguyentrongdupl@gmail.com>
2021-12-29 17:18:01 +0700

    Fix x button close program

--- School Project/Mainwindow.Designer.cs
+++ School Project/Mainwindow.Designer.cs
@@ -62,7 +62,7 @@ namespace School_Project
             this.Manage_Students.Image = ((System.Drawing.Image)(resources.GetObject("Manage_Students.Image")));
             this.Manage_Students.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.Manage_Students.Name = "Manage_Students";
-            this.Manage_Students.Size = new System.Drawing.Size(136, 32);
+            this.Manage_Students.Size = new System.Drawing.Size(170, 32);
             this.Manage_Students.Text = "Quản lý học sinh";
             this.Manage_Students.Click += new System.EventHandler(this.Manage_Students_Click);
             // 
@@ -72,7 +72,7 @@ namespace School_Project
             this.Manage_Staff.Image = ((System.Drawing.Image)(resources.GetObject("Manage_Staff.Image")));
             this.Manage_Staff.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.Manage_Staff.Name = "Manage_Staff";
-            this.Manage_Staff.Size = new System.Drawing.Size(141, 32);
+            this.Manage_Staff.Size = new System.Drawing.Size(175, 32);
             this.Manage_Staff.Text = "Quản lý giáo viên";
             this.Manage_Staff.Click += new System.EventHandler(this.Manage_Staff_Click);
             // 
@@ -82,7 +82,7 @@ namespace School_Project
             this.Manage_Marks.Image = ((System.Drawing.Image)(resources.GetObject("Manage_Marks.Image")));
             this.Manage_Marks.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.Manage_Marks.Name = "Manage_Marks";
-            this.Manage_Marks.Size = new System.Drawing.Size(128, 32);
+            this.Manage_Marks.Size = new System.Drawing.Size(154, 32);
             this.Manage_Marks.Text = "Manage Marks";
             this.Manage_Marks.Click += new System.EventHandler(this.Manage_Marks_Click);
             // 
@@ -92,7 +92,7 @@ namespace School_Project
             this.Manage_Users.Image = ((System.Drawing.Image)(resources.GetObject("Manage_Users.Image")));
             this.Manage_Users.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.Manage_Users.Name = "Manage_Users";
-            this.Manage_Users.Size = new System.Drawing.Size(156, 32);
+            this.Manage_Users.Size = new System.Drawing.Size(195, 32);
             this.Manage_Users.Text = "Quản lý người dùng";
             this.Manage_Users.Click += new System.EventHandler(this.Manage_Users_Click);
             // 
@@ -102,13 +102,13 @@ namespace School_Project
             this.Log_out.Image = ((System.Drawing.Image)(resources.GetObject("Log_out.Image")));
             this.Log_out.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.Log_out.Name = "Log_out";
-            this.Log_out.Size = new System.Drawing.Size(99, 32);
+            this.Log_out.Size = new System.Drawing.Size(121, 32);
             this.Log_out.Text = "Đăng xuất";
             this.Log_out.Click += new System.EventHandler(this.Log_out_Click);
             // 
             // Mainwindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(876, 609);
@@ -118,7 +118,7 @@ namespace School_Project
             this.Name = "Mainwindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Trang chủ";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Mainwindow_FormClosed);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Mainwindow_FormClosing);
             this.Load += new System.EventHandler(this.Mainwindow_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
